02678dd2b7d308df370ff3704230b9df0d60014f
pedro suarez <pssuarez1402@gmail.com>
2023-11-17 16:28:10 -0500

    cambios realizados para la pages home, faltaria arreglar para que las cards sean traidas de la base de datos y que el boton nos lleve la siguiente vista

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,16 +1,192 @@
+/* import React from 'react'; */
+
 const Home = () => {
   return (
     <main className="container">
-        <section className="card">
-            <div className="card-header">
-                Errores
+      <div className="row mt-5">
+        <div className="col-md-4">
+          <div className="card mb-4">
+            <div className="card-header text-center">
+              <h2>Errores Reportados</h2>
             </div>
             <div className="card-body">
-                
+              <div className="mb-3">
+                <h5 className="card-title">Nombre del equipo: </h5>
+                <h6 className="card-subtitle mb-2 text-muted">Sala ubicada:</h6>
+                <h6 className="card-subtitle mb-2 text-muted">Tipo de equipo:</h6>
+                <p className="card-text">Indicencia:</p>
+                <button className="btn btn-primary">Gestionar</button>
+              </div>
             </div>
-        </section>
+          </div>
+
+          {/* Agrega más tarjetas aquí según sea necesario */}
+          <div className="card mb-4">
+            <div className="card-header text-center">
+              <h2>Errores Reportados</h2>
+            </div>
+            <div className="card-body">
+              <div className="mb-3">
+                <h5 className="card-title">Nombre del equipo: </h5>
+                <h6 className="card-subtitle mb-2 text-muted">Sala ubicada:</h6>
+                <h6 className="card-subtitle mb-2 text-muted">Tipo de equipo:</h6>
+                <p className="card-text">Indicencia:</p>
+                <button className="btn btn-primary">Gestionar</button>
+              </div>
+            </div>
+          </div>
+          <div className="card mb-4">
+            <div className="card-header text-center">
+              <h2>Errores Reportados</h2>
+            </div>
+            <div className="card-body">
+              <div className="mb-3">
+                <h5 className="card-title">Nombre del equipo: </h5>
+                <h6 className="card-subtitle mb-2 text-muted">Sala ubicada:</h6>
+                <h6 className="card-subtitle mb-2 text-muted">Tipo de equipo:</h6>
+                <p className="card-text">Indicencia:</p>
+                <button className="btn btn-primary">Gestionar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="card mb-4">
+            <div className="card-header text-center">
+              <h2>Errores Reportados</h2>
+            </div>
+            <div className="card-body">
+              <div className="mb-3">
+                <h5 className="card-title">Nombre del equipo: </h5>
+                <h6 className="card-subtitle mb-2 text-muted">Sala ubicada:</h6>
+                <h6 className="card-subtitle mb-2 text-muted">Tipo de equipo:</h6>
+                <p className="card-text">Indicencia:</p>
+                <button className="btn btn-primary">Gestionar</button>
+              </div>
+            </div>
+          </div>
+
+          {/* Agrega más tarjetas aquí según sea necesario */}
+          <div className="card mb-4">
+            <div className="card-header text-center">
+              <h2>Errores Reportados</h2>
+            </div>
+            <div className="card-body">
+              <div className="mb-3">
+                <h5 className="card-title">Nombre del equipo: </h5>
+                <h6 className="card-subtitle mb-2 text-muted">Sala ubicada:</h6>
+                <h6 className="card-subtitle mb-2 text-muted">Tipo de equipo:</h6>
+                <p className="card-text">Indicencia:</p>
+                <button className="btn btn-primary">Gestionar</button>
+              </div>
+            </div>
+          </div>
+          <div className="card mb-4">
+            <div className="card-header text-center">
+              <h2>Errores Reportados</h2>
+            </div>
+            <div className="card-body">
+              <div className="mb-3">
+                <h5 className="card-title">Nombre del equipo: </h5>
+                <h6 className="card-subtitle mb-2 text-muted">Sala ubicada:</h6>
+                <h6 className="card-subtitle mb-2 text-muted">Tipo de equipo:</h6>
+                <p className="card-text">Indicencia:</p>
+                <button className="btn btn-primary">Gestionar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="card mb-4">
+            <div className="card-header text-center">
+              <h2>Errores Reportados</h2>
+            </div>
+            <div className="card-body">
+              <div className="mb-3">
+                <h5 className="card-title">Nombre del equipo: </h5>
+                <h6 className="card-subtitle mb-2 text-muted">Sala ubicada:</h6>
+                <h6 className="card-subtitle mb-2 text-muted">Tipo de equipo:</h6>
+                <p className="card-text">Indicencia:</p>
+                <button className="btn btn-primary">Gestionar</button>
+              </div>
+            </div>
+          </div>
+
+          {/* Agrega más tarjetas aquí según sea necesario */}
+          <div className="card mb-4">
+            <div className="card-header text-center">
+              <h2>Errores Reportados</h2>
+            </div>
+            <div className="card-body">
+              <div className="mb-3">
+                <h5 className="card-title">Nombre del equipo: </h5>
+                <h6 className="card-subtitle mb-2 text-muted">Sala ubicada:</h6>
+                <h6 className="card-subtitle mb-2 text-muted">Tipo de equipo:</h6>
+                <p className="card-text">Indicencia:</p>
+                <button className="btn btn-primary">Gestionar</button>
+              </div>
+            </div>
+          </div>
+          <div className="card mb-4">
+            <div className="card-header text-center">
+              <h2>Errores Reportados</h2>
+            </div>
+            <div className="card-body">
+              <div className="mb-3">
+                <h5 className="card-title">Nombre del equipo: </h5>
+                <h6 className="card-subtitle mb-2 text-muted">Sala ubicada:</h6>
+                <h6 className="card-subtitle mb-2 text-muted">Tipo de equipo:</h6>
+                <p className="card-text">Indicencia:</p>
+                <button className="btn btn-primary">Gestionar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="card mb-4">
+            <div className="card-header text-center">
+              <h2>Errores Reportados</h2>
+            </div>
+            <div className="card-body">
+              <div className="mb-3">
+                <h5 className="card-title">Nombre del equipo: </h5>
+                <h6 className="card-subtitle mb-2 text-muted">Sala ubicada:</h6>
+                <h6 className="card-subtitle mb-2 text-muted">Tipo de equipo:</h6>
+                <p className="card-text">Indicencia:</p>
+                <button className="btn btn-primary">Gestionar</button>
+              </div>
+            </div>
+          </div>
+
+          {/* Agrega más tarjetas aquí según sea necesario */}
+          
+        </div>
+
+        <div className="col-md-4">
+          <div className="card mb-4">
+            <div className="card-header text-center">
+              <h2>Errores Reportados</h2>
+            </div>
+            <div className="card-body">
+              <div className="mb-3">
+                <h5 className="card-title">Nombre del equipo: </h5>
+                <h6 className="card-subtitle mb-2 text-muted">Sala ubicada:</h6>
+                <h6 className="card-subtitle mb-2 text-muted">Tipo de equipo:</h6>
+                <p className="card-text">Indicencia:</p>
+                <button className="btn btn-primary">Gestionar</button>
+              </div>
+            </div>
+          </div>
+
+          {/* Agrega más tarjetas aquí según sea necesario */}
+          
+        </div>
+      </div>
     </main>
-  )
+  );
 }
 
-export default Home
+export default Home;
