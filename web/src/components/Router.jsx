8dbfc478ef8f80a4958b0eabce40dac9e5f210f2
pedro suarez <pssuarez1402@gmail.com>
2023-11-20 11:59:55 -0500

    Se ordena la estructura del codigo segun los criterios del profesor

--- web/src/components/Router.jsx
+++ web/src/components/Router.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
-import IssueManage from "../pages/IssueManage";
+import Home from "./pages/Home";
+import IssueManage from "./pages/IssueManage";
 import Header from "./Header";
 
 const Router = () => {
