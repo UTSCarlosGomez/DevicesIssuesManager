5008d2ccf8d106d165f50c4f9f571c976c7c47ac
PedroSuarez1402 <pssuarez1402@gmail.com>
2024-08-28 12:17:22 -0500

    redireccionamiento al login

--- web/src/components/header/AppHeaderDropdown.js
+++ web/src/components/header/AppHeaderDropdown.js
@@ -5,10 +5,16 @@ import { CAvatar, CBadge, CDropdown, CDropdownDivider, CDropdownHeader, CDropdow
 import { cilBell, cilCreditCard, cilCommentSquare, cilEnvelopeOpen, cilFile, cilLockLocked, cilSettings, cilTask, cilUser,
 } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
+import { useNavigate } from 'react-router-dom'
 
 import avatar8 from './../../assets/images/avatars/8.jpg'
 
 const AppHeaderDropdown = () => {
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    navigate('login')
+  }
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0 pe-0" caret={false}>
@@ -25,7 +31,7 @@ const AppHeaderDropdown = () => {
           Settings
         </CDropdownItem>
         <CDropdownDivider />
-        <CDropdownItem href="/login">
+        <CDropdownItem onClick={handleLogout}>
           <CIcon icon={cilLockLocked} className="me-2" />
           Log Out
         </CDropdownItem>
